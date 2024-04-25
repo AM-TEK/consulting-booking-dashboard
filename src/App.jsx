7b948db7151c1b85d04246382ce81ef64ebe0edd
Alejandro Maldonado <am@AM-WEBDEV.local>
2024-04-25 12:01:06 -0500

    add pages and routes, start on appLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,34 @@
-import styled from "styled-components"
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button"
-import Input from "./ui/Input"
-import Heading from "./ui/Heading"
-import Row from "./ui/Row"
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Rooms from "./pages/Rooms";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import AppLayout from "./ui/AppLayout";
 
-
-const StyledApp = styled.div`
-  background-color: beige;
-  padding: 20px;
-`;
-
-function App() {
+export default function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row type="vertical">
-          <Row type="horizontal">
-            <Heading as="h1">Consultancy Dashboard</Heading>
-            <div>
-              <Heading as="h2">Book a room at our offices or virtually</Heading>
-              <Button>Check in</Button>
-              <Button variation="secondary" size="small">Check out</Button>
-            </div>
-          </Row>
-          <Row type="vertical">
-            <Heading as="h3">Form</Heading>
-            <form action="">
-              <Input type="number" placeholder="Number of clients" />
-              <Input type="number" placeholder="Number of clients" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />}/>
+            <Route path="dashboard" element={<Dashboard />}/>
+            <Route path="account" element={<Account />}/>
+            <Route path="bookings" element={<Bookings />}/>
+            <Route path="rooms" element={<Rooms />}/>
+            <Route path="settings" element={<Settings />}/>
+            <Route path="users" element={<Users />}/>
+          </Route>
+          <Route path="login" element={<Login />}/>
+          <Route path="*" element={<PageNotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
-
-export default App
